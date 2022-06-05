f12a68a57ef146b9b07454463567680ae64db476
N4h0m <nahomberhane56@gmail.com>
2022-06-05 13:30:43 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-* Description: main - Prints out a string to stdout.
-* Return: 0 if success
-* */
+* main - Entry point
+* 
+* Return: Always 0 (success)
+* /
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"
-			return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+			return (0):
 			}
